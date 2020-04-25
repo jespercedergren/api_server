d4f2a9e89d6860ce0795f7eecbff87e24b5d2d29
Jesper Cedergren <jespercedergren@Jespers-MacBook-Pro.local>
2020-04-25 10:48:39 +0200

    run_all_tests.sh

--- tests/docker_tests/run_all_tests.sh
+++ tests/docker_tests/run_all_tests.sh
@@ -9,12 +9,10 @@ docker-compose -f ../server/docker-compose.yml up -d
 sleep 10
 
 # unit tests
-#docker run -it -e test_env='docker' --network api_default --mount type=bind,src="$(pwd)/../../clients",target=/clients --mount type=bind,src="$(pwd)/..",target=/tests test_base python3.7 -m pytest tests/unit_tests/
+docker run -it -e test_env='docker' --network api_default --mount type=bind,src="$(pwd)/../../clients",target=/clients --mount type=bind,src="$(pwd)/..",target=/tests test_base python3.7 -m pytest tests/unit_tests/
 
 # integration tests
-#docker run -it -e test_env='docker' --network api_default --mount type=bind,src="$(pwd)/..",target=/tests test_base python3.7 -m pytest tests/integration_tests/
-docker run -it -e test_env='docker' --network api_default --mount type=bind,src="$(pwd)/..",target=/tests test_base python3.7 -m pytest tests/integration_tests/api/test_api_read_s3.py -s
-docker run -it -e test_env='docker' --network api_default --mount type=bind,src="$(pwd)/..",target=/tests test_base python3.7 -m pytest tests/integration_tests/api/test_api_read_dynamodb.py -s
+docker run -it -e test_env='docker' --network api_default --mount type=bind,src="$(pwd)/..",target=/tests test_base python3.7 -m pytest tests/integration_tests/
 
 # spin down services
 docker-compose -f ../server/docker-compose.yml down --remove-orphans
