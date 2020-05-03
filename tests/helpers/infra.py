5de4103d0d3323cb36e10d0c35ec4e8910403070
Jesper Cedergren <jespercedergren@Jespers-MacBook-Pro.local>
2020-05-03 17:07:40 +0200

    Removed comments.

--- tests/helpers/infra.py
+++ tests/helpers/infra.py
@@ -1,7 +1,4 @@
 import boto3
-#from mypy_boto3 import s3
-#from mypy_boto3 import secretsmanager
-#from mypy_boto3 import firehose
 import mypy_boto3_s3 as s3
 import mypy_boto3_firehose as firehose
 import mypy_boto3_secretsmanager as secretsmanager
