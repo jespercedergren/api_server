7ce7843b70fda8c0f1ec6abe2db7937e74069d08
Thelin90 <simon@gameanalytics.com>
2020-05-09 23:21:09 +0100

    APISERVER-001-ADD-USER-AGENT-STRING
    *NOTES*
    
    * https://developers.whatismybrowser.com/useragents/explore/software_name/okhttp/

--- api/web/app.py
+++ api/web/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
-
+from typing import Dict
+from json import dumps
 from clients.firehose import FirehoseClient
 from clients.mongo import MongoDBClient
 from clients.s3 import S3Client, S3ParquetClient
@@ -10,13 +11,28 @@ app = Flask(__name__)
 
 # Assuming environment is set here
 
+
+def _extract_user_agent(data_json: Dict[str, str]) -> None:
+    """Private inner function to extract header data from user agent string
+
+    :param data_json (Dict[str, str]): Flask data json response
+    """
+    data_json["user_agent_string"] = request.user_agent.string
+    data_json["user_agent_platform"] = request.user_agent.platform
+    data_json["user_agent_browser"] = request.user_agent.browser
+    data_json["user_agent_version"] = request.user_agent.version
+    data_json["user_agent_language"] = request.user_agent.language
+
+
 # POST
 @app.route("/api/ingest_data/mongo", methods=["POST"])
 def ingest_json_mongo():
     """
     :return: json
     """
-    data_json = request.json
+    data_json = request.get_json(force=True)
+    _extract_user_agent(data_json)
+
     mongo_client = MongoDBClient(database="test", collection="test")
     object_id_1 = mongo_client.save(data_json)
     return jsonify({"Data ingested": f"{object_id_1}"})
@@ -27,9 +43,11 @@ def ingest_bytes_json_firehose():
     """
     :return: json
     """
-    data_bytes = request.data
+    data_json = request.get_json(force=True)
+    _extract_user_agent(data_json)
+
     firehose_client = FirehoseClient(delivery_stream_name="api_to_s3_ingest")
-    response = firehose_client.put_record(record=data_bytes)
+    response = firehose_client.put_record(record=dumps(data_json).encode('utf-8'))
     return jsonify({"Data ingested": f"{response}"})
 
 
@@ -38,9 +56,15 @@ def ingest_bytes_json_smoke_test():
     """
     :return: json
     """
-    data_bytes = request.data
+    data_json = request.get_json(force=True)
+    _extract_user_agent(data_json)
+
     s3_client = S3Client(bucket="test-bucket")
-    response = s3_client.upload_record(record=data_bytes, get_key_func=get_firehose_output_s3_key, prefix="test-prefix")
+    response = s3_client.upload_record(
+        record=dumps(data_json).encode('utf-8'),
+        get_key_func=get_firehose_output_s3_key,
+        prefix="test-prefix",
+    )
     return jsonify({'Data ingested': f'{response}'})
 
 
